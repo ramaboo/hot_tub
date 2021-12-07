2458e87af6cfe7b1ae592194575d2f8f417af62d
David <david@ramaboo.com>
2021-12-07 00:50:45 -0800

    first

---
+++ flask/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World! 5" + os.environ.get("DOMAIN")
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="0.0.0.0")
